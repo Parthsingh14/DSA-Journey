e99b60ef4d662683c9c335631c6c3468eb9e9120
Parth Singh <parthsingh503@gmail.com>
2025-02-25 11:07:22 +0530

    Add user input functionality to Basics.java

--- 1. Basics/Basics.java
+++ 1. Basics/Basics.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Basics{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        //1. User Input Output
+        System.out.println("Hello World");
+
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        System.out.println(n);
+        scanner.close();
     }
 }
